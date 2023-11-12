baf3eb81be07a5a5a6e9fa9118f76591ee039054
amankanz <akanezamulume.iuea@gmail.com>
2023-11-12 20:39:42 +0300

    portfolio page

---
+++ user-portfolio/src/App.jsx
@@ -0,0 +1,10 @@
+
+import Portfolio from "./my-portfolio";
+
+function App() {
+  return (
+    <div><Portfolio /></div>
+  )
+}
+
+export default App;
---
+++ user-portfolio/src/my-portfolio.jsx
@@ -0,0 +1,29 @@
+
+function Portfolio() {
+  return (
+    <>
+      <h1>Hellooo World</h1>
+      <h2>Full Name:</h2>
+      <p><a href="https://github.com/amankanz" target="_blanl">Fred Kaneza</a>, Boot Camp Student on Coding Adventure</p>
+
+      <h2>Educational Background:</h2>
+      <p>Currently attending a frontend development boot camp at <a href="https://www.azubiafrica.org/" target="_blank" rel="noreferrer">Azubi Africa</a></p>
+
+      <h2>Professional Background:</h2>
+      <p>Experienced in computer repair and maintenance</p>
+
+      <h2>Professional Background:</h2>
+      <p>Experienced in computer repair and maintenance</p>
+
+      <h2>Skills:</h2>
+      <ul>
+        <li>Proficient in HTML, CSS, and JavaScript</li>
+        <li>Currently learning React for frontend development</li>
+      </ul>
+
+      <div>Learn more <a href="https://www.linkedin.com/in/fred-kaneza/" target="_blank" rel="noreferrer">about Me</a>, Happy Coding!</div>
+    </>
+  )
+}
+
+export default Portfolio;
